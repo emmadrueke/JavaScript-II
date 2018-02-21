ad4e0bf8646f0ffeeb50bdd924a3a5e53f8662b1
Emma Drueke <emma.drueke@gmaila.com>
2018-02-21 17:18:38 -0600

    final push

--- src/this.js
+++ src/this.js
@@ -11,8 +11,8 @@ class User {
     this.password = options.password;
   }
   checkPassword(str) {
-    return str === this.password;  
-  };
+    return str === this.password;
+  }
   // create a method on the User class called `checkPassword`
   // this method should take in a string and compare it to the object's password property
   // return `true` if they match, othrwise return `false`
@@ -23,15 +23,15 @@ const me = new User({
   password: 'correcthorsebatterystaple',
 });
 
-const result = me.checkPassword('correcthorsebatterystaple'); // should return `true`
-console.log(result);
+const result = me.checkPassword('correcthorsebatterystaple');// should return `true`
+// console.log(result);
 /* part 2 */
 const checkPassword = function comparePasswords(passwordToCompare) {
   // recreate the `checkPassword` method that you made on the `User` class
   // use `this` to access the object's `password` property.
   // do not modify this function's parameters
   // note that we use the `function` keyword and not `=>`
-  console.log('Im really doing it');
+  // console.log('Im really doing it');
   return this.password === passwordToCompare;
 };
 checkPassword.call(me, 'correcthorsebatterystaple');
